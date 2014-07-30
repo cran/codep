30c047936ef8ed34974112d8f6939111bd60aa43
Guillaume Guenard <guillaume.guenard@gmail.com>
2014-07-30 00:00:00 +0000

    version 0.4-1

--- R/eigenmap.R
+++ R/eigenmap.R
@@ -1,56 +1,123 @@
 #
-eigenmap <- function(x,opt.coord=NA,truncate=c(0,NA),wf,wpar,select=.Machine$double.eps^0.5) {
-  #
-  if(missing(wf)) stop("Parameter 'wf' must be one of 'binary', 'dbMEM', 'Drayf1', 'Drayf2', 'Drayf3', or 'sqrd'")
+eigenmap <- function(x,opt.coord=NA,weighting=Wf.sqrd,boundaries,wpar,select=.Machine$double.eps^0.5) {
+#
+  if(!is.function(weighting)) stop("Parameter 'weighting' must be a weighting function.")
   if(!is.numeric(x)) stop("Parameter 'x' must be numeric!")
-  wf <- match.arg(wf,c("binary","dbMEM","Drayf1","Drayf2","Drayf3","sqrd"))
-  if(((wf=="Drayf2")||(wf=="Drayf3"))&&missing(wpar))
-    stop("Parameter 'wpar' is mandatory with wf functions Drayf2 or Drayf3!")
   if(inherits(x,"dist")) {
-    dd <- as.matrix(x)
+    D <- as.matrix(x)
     if(all(is.na(opt.coord))) {
-      opt.coord <- as.matrix(x=1:nrow(dd))
-      rownames(opt.coord) <- rownames(dd)
+      opt.coord <- as.matrix(x=1:nrow(D))
+      rownames(opt.coord) <- rownames(D)
     } else {
       opt.coord <- as.matrix(opt.coord)
-      if(nrow(opt.coord) != nrow(dd)) {
-        stop(paste("You provided",nrow(opt.coord),"optional coordinates to reference", nrow(dd),"observations!"))
-      } else rownames(opt.coord) <- rownames(dd)
+      if(nrow(opt.coord) != nrow(D)) {
+        stop(paste("You provided",nrow(opt.coord),"optional coordinates to reference", nrow(D),"observations!"))
+      } else rownames(opt.coord) <- rownames(D)
     }
   } else {
     opt.coord <- as.matrix(x)
-    x <- dist(x,method="euclidean") ; dd <- as.matrix(x)
-    rownames(opt.coord) <- rownames(dd)    
+    x <- dist(x,method="euclidean")
+    D <- as.matrix(x)
+    rownames(opt.coord) <- rownames(D)
   }
-  if(wf[1] == "sqrd") {
-    W <- -0.5*dd ; b <- NULL ; a <- NULL ; truncate <- NULL
+  wformals <- names(formals(weighting))
+  if(any(wformals == "boundaries") && missing(boundaries)) {
+    boundaries <- c(0,max(hclust(x,method="single")$height))
+    warning("No boundaries given, they were set to (",boundaries[1L],"; ",boundaries[2L],")")
+  }
+  if(any(wformals == "boundaries")) {
+    if(any(wformals == "wpar") && !missing(wpar))
+      W <- weighting(D,boundaries,wpar)
+    else
+      W <- weighting(D,boundaries)
   } else {
-    if(any(is.na(truncate)) || length(truncate) != 2L) truncate <- c(0,max(hclust(x,method="single")$height))
-    b <- matrix(0,nrow(dd),ncol(dd)) ; b[dd >= truncate[1L] & dd <= truncate[2]] <- 1
-    if (wf == "binary") {
-      W <- b ; a <- NULL
-    } else {
-      if (wf == "dbMEM") a <- 1-(dd/(4*truncate[2L]))^2
-      if (wf == "Drayf1") a <- 1-dd/max(dd)
-      if (wf == "Drayf2") a <- 1-(dd/max(dd))^wpar
-      if (wf == "Drayf3") a <- 1/dd^wpar
-      W <- b * a
-    }
-    diag(W) <- 0
+    if(any(wformals == "wpar") && !missing(wpar))
+      W <- weighting(D,wpar)
+    else
+      W <- weighting(D)
   }
-  n <- nrow(W) ; term <- diag(n) - (cbind(rep(1,n)) %*% rbind(rep(1,n)))/n ; O <- term %*% W %*% term
+  diag(W) <- 0
+  n <- nrow(W)
+  colWbar <- matrix(colMeans(W),1L,n)
+  MW <- mean(W)
+  term <- matrix(1,n,1L) %*% colWbar
+  O <- W - term - t(term) + MW
   eigO <- eigen(O)
   variables <- eigO$vectors[,abs(eigO$values) >= select]
-  rownames(variables) <- rownames(dd) ; colnames(variables) <- paste("MEM",1L:ncol(variables),sep="")
+  rownames(variables) <- rownames(D) ; colnames(variables) <- paste("dbMEM",1L:ncol(variables),sep="")
   return(structure(list(
     coordinates=opt.coord,
-    truncate=truncate,D=dd,weighting=wf,
-    wpar=if((wf=="Drayf2")||(wf=="Drayf3")) wpar else NULL,
+    D=D,weighting=weighting,
+    boundaries=if(!missing(boundaries)) boundaries else NULL,
+    wpar=if(!missing(wpar)) wpar else NULL,
+    W=W,colWbar=colWbar,MW=MW,
     lambda=eigO$values[abs(eigO$values) >= select],
     U=variables),class="eigenmap"))
 }
 #
-print.eigenmap <- function(x, ...) {
+Wf.sqrd <- function(D) return(-0.5 * D)
+#
+Wf.binary <- function(D,boundaries) {
+  b <- matrix(0, nrow(D), ncol(D))
+  b[D > boundaries[1L] & D <= boundaries[2L]] <- 1
+  return(b)
+}
+#
+Wf.PCNM <- function(D,boundaries) {
+  b <- Wf.binary(D, boundaries)
+  a <- 1 - (D / (4 * boundaries[2L]))**2
+  W <- b * a
+  return(W)
+}
+#
+Wf.Drayf1 <- function(D,boundaries) {
+  b <- Wf.binary(D, boundaries)
+  a <- 1 - D / max(D)
+  W <- b * a
+  return(W)
+}
+#
+Wf.Drayf2 <- function(D,boundaries,wpar=1) {
+  b <- Wf.binary(D, boundaries)
+  a <- 1 - (D / max(D))^wpar
+  W <- b * a
+  return(W)
+}
+#
+Wf.Drayf3 <- function(D,boundaries,wpar=1) {
+  b <- Wf.binary(D, boundaries)
+  a <- 1 / D^wpar
+  W <- b * a
+  return(W)
+}
+#
+eigenmap.score <- function(object,target) {
+  if(!is.matrix(target)) attr(target,"dim") <- c(1L,length(target))
+  n <- ncol(object$D)
+  if(n != ncol(target))
+    stop("Mismatch distances to target: ",n," but ",ncol(object$D)," expected.")
+  ntgt <- nrow(target)
+  #
+  if(!is.null(object$boundaries)) {
+    if(!is.null(object$wpar))
+      Wnk <- object$weighting(target,object$boundaries,object$wpar)
+    else
+      Wnk <- object$weighting(target,object$boundaries)
+  } else {
+    if(!is.null(object$wpar))
+      Wnk <- object$weighting(target,object$wpar)
+    else
+      Wnk <- object$weighting(target)
+  }
+  Wnk[target==0] <- 0
+  scores <- (Wnk - matrix(rowMeans(Wnk),ntgt,1L) %*% matrix(1,1L,n) -
+             matrix(1,ntgt,1L) %*% object$colWbar + object$MW) %*%
+             object$U %*% diag(object$lambda**(-1))
+  colnames(scores) <- colnames(object$U)
+  return(scores)
+}
+#
+print.eigenmap <- function(x,...) {
   cat(paste("\nMoran's eigenvector map containing",length(x$lambda),"basis functions.\n"))
   cat(paste("Functions span",nrow(x$U),"observations.\n\n"))
   cat(paste("Eigenvalues:\n"))
@@ -59,7 +126,7 @@ print.eigenmap <- function(x, ...) {
   return(invisible(NULL))
 }
 #
-plot.eigenmap <- function(x, ...) {
+plot.eigenmap <- function(x,...) {
   if (ncol(x$coordinates) > 2) {
     warning(paste(ncol(x$coordinates),"dimensions were provided but only the first 2 were used for plotting."))
   }
